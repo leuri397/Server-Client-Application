b5b0b16891884d8a6908fa088dc86968ee11ad73
leuri397-Linux <leuri2001@gmail.com>
2021-02-13 13:59:44 +0300

    Removed CMake files

--- Common/CMakeFiles/common.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/common.dir/BERlength.cpp.o"
-  "CMakeFiles/common.dir/ClientHandler.cpp.o"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/common.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
